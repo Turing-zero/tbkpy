e20736e8e769c85be3e002c9a4337f592e27e2e2
mark <mark_zjunlict@zju.edu.cn>
2024-09-12 14:43:33 +0800

    add callback & plugin for udp

--- src/tbkpy/socket/udp.py
+++ src/tbkpy/socket/udp.py
@@ -1,44 +1,85 @@
 import sys
 import socket
 import struct
+import threading
 
-class UDPMultiCastReceiver:
-    def __init__(self, group, port):
-        self.group = group
-        self.port = port
+class UDPReceiver:
+    def __init__(self, port, *, bind_ip='', multicast=False, multicast_group=None, callback=None, plugin=None):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind(('', port))
-        mreq = struct.pack("4sl", socket.inet_aton(group), socket.INADDR_ANY)
-        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    
+        self.sock.settimeout(1)
+        self.sock.bind((bind_ip, port))
+        if multicast:
+            mreq = struct.pack("4sl", socket.inet_aton(multicast_group), socket.INADDR_ANY)
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        if callback:
+            self.callback = callback if not plugin else lambda x: callback((plugin.decode(x[0]),x[1]))
+            self.stop_token = threading.Event()
+            self.recvThread = threading.Thread(target=self.__recv)
+            self.recvThread.daemon = True
+            self.recvThread.start()
+
     def recv(self):
         try:
             return True, self.sock.recvfrom(65536)
         except socket.error as e:
             return False, None
+    
+    def __recv(self):
+        while True:
+            res, recv = self.recv()
+            if self.stop_token.is_set():
+                break
+            if res:
+                self.callback(recv)
+    def stop(self):
+        self.stop_token.set()
+        self.recvThread.join()
 
     def setblocking(self, blocking):
         self.sock.setblocking(blocking)
 
-class UDPMultiCastSender:
-    def __init__(self):
+class UDPMultiCastReceiver(UDPReceiver):
+    def __init__(self, group, port):
+        super().__init__(port, multicast=True, multicast_group=group)
+
+class UDPSender:
+    def __init__(self, *, multicast=False, ttl=2, multicast_interface=None):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-        # self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
-        # self.sock.bind(('', 0))
+        if multicast:
+            self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+            if multicast_interface:
+                self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(multicast_interface))
     
     def send(self, msg, ep):
         self.sock.sendto(msg, ep)
 
+class UDPMultiCastSender(UDPSender):
+    def __init__(self, *, ttl=2, multicast_interface=None):
+        super().__init__(multicast=True, ttl=ttl, multicast_interface=multicast_interface)
     def setInterface(self, ip):
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(ip))
 
 if __name__ == "__main__":
-    if len(sys.argv)>1 and sys.argv[1] == "send":
-        s = UDPMultiCastSender()
-        s.send("Hello".encode(), ("233.233.233.233", 12321))
-    else:
-        d = UDPMultiCastReceiver("233.233.233.233", 12321)
+
+    def test1():
+        if len(sys.argv)>1 and sys.argv[1] == "send":
+            s = UDPMultiCastSender()
+            s.send("Hello".encode(), ("233.233.233.233", 12321))
+        else:
+            d = UDPMultiCastReceiver("233.233.233.233", 12321)
+            while True:
+                print(d.recv())
+    
+    def test2():
+        import time
+        from tbkpy.socket.plugins import ProtobufParser
+        from tzcp.ssl.rocos.zss_vision_detection_pb2 import Vision_DetectionFrame
+        def cb(v):
+            print("in cb : ",v)
+        d = UDPReceiver(23333, callback=cb, plugin=ProtobufParser(Vision_DetectionFrame))
         while True:
-            print(d.recv())
+            time.sleep(1)
+            print("...")
+    test2()
